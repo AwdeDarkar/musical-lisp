9f73f5aba59314a7d3350e179c951b06d58c5698
AwdeDarkar <darkardengeno@gmail.com>
2021-02-22 17:00:20 -0500

    Get a very simple version of the audio play method

--- base.lisp
+++ base.lisp
@@ -6,41 +6,54 @@
 (defconstant +output-file+ "/home/darkar/bench/learning/lisp/musical-lisp/generated/output.wav")
 (defconstant +sample-rate+ 44100)
 
-(defun make-phase-generator (sample-rate)
-    (let ((phi 0.0))
-        (lambda (frequency)
-            (setf phi (rem (+ phi (/ (* 2 PI frequency) sample-rate)) (* 2 PI)))
-            phi
-        )
-    )
-)
-
-(defun sine-tone (duration frequency volume wave-writer phase-generator)
+(defun play-function (func duration volume wave-writer) 
   (dotimes (i (* duration +sample-rate+))
-    (let ((phase (funcall phase-generator frequency)))
-      ;; Channel 1
-      (funcall (getf wave-writer :write-sample) (* volume (sin phase)))
-      ;; Channel 2
-      (funcall (getf wave-writer :write-sample)  (* volume (cos phase)))
-      )
-    )
+      (funcall (getf wave-writer :write-sample) (* volume (funcall func (/ i +sample-rate+))))
+      (funcall (getf wave-writer :write-sample) (* volume (funcall func (/ i +sample-rate+))))
+  )
+ )
+
+(defun make-sine-tone (frequency offset volume)
+  (lambda (tm)
+    (* volume (sin (+ (* 2 PI frequency tm) offset)))
+  )
+ )
+
+(defun play-sine-tone (duration frequency volume wave-writer)
+  (play-function (make-sine-tone frequency 0 1) duration volume wave-writer)
   )
 
-(defun example()
-  (let* ((sample-rate +sample-rate+)
-         (phase-generator (make-phase-generator sample-rate)))
-    ;; Instantiate writer
-    (let ((wave-writer (cl-wave-file-writer:make-writer
-                        :filename +output-file+
-                        :channel-count 2
-                        :sample-width :16Bit
-                        :sample-rate sample-rate)))
+(defclass playable-tone ()
+  ((frequency
+     :initarg :frequency
+     :accessor frequency)
+   (duration
+     :initarg :duration
+     :accessor duration))
+  )
+
+(defmethod play-tone (playable volume wave-writer)
+  (play-sine-tone (slot-value playable 'duration) (slot-value playable 'frequency) volume wave-writer)
+  )
+
+(defun main()
+  ;; Instantiate writer
+  (let ((wave-writer (cl-wave-file-writer:make-writer
+                       :filename +output-file+
+                       :channel-count 2
+                       :sample-width :16Bit
+                       :sample-rate +sample-rate+)))
       ;; Open file
       (funcall (getf wave-writer :open-file))
       ;; Write samples
-      (sine-tone 2 640 0.04 wave-writer phase-generator)
+      (play-tone
+        (make-instance 'playable-tone :frequency 440 :duration 2)
+        0.04
+        wave-writer
+        )
       ;; Close file
-      (funcall (getf wave-writer :close-file))))
-  "DONE")
+      (funcall (getf wave-writer :close-file))
+   )
+ )
 
-(example)
+(main)
